98215d0d2dbc044b8ad3d3eb8c5133f3c43493c5
monstermiles <miles.emmitt.moss@gmail.com>
2021-12-20 18:33:09 -0600

    generate markdown function

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs');
+const path = require('path')
 const generateMarkdown = require('./utils/generateMarkdown')
 
 
@@ -78,108 +79,8 @@ inquirer.prompt(questions)
 
         const fileName = `${answer.title}.md `
 
-        // // TODO: Create a function to generate markdown for README
-        const markdownContent =
-            `
-        ${answer.title}
-        ${(renderLicenseBadge(answer.license))}
-
-
-        ## Table of Contents:
-        1. [Description](#description) 
-        2. [Installation](#installation)
-        3. [Usage](#usage)
-        4. [Contributing](#contributing)
-        5. [Testing](#testing)
-        6. [License](#license)
-        7. [Questions](#questions)
-
-
-        ## Description
-        ${answer.description}
-
-
-        ## Installation
-        ${answer.installation}
-
-        
-        ## Usage
-        ${answer.usage}
-
-
-        ## Contributing
-        ${answer.contributing}
-
-        
-        ## Testing
-        ${answer.testing}
-
-
-        ## License
-        ${renderLicenseBadge(answer.license)}
-        ${renderLicenseSection(answer.license)}
-
-        
-        ## Questions
-        Contact me here with any questions about this project:
-        Github: ${answer.github}
-        Email: ${answer.email}
-
-        `;
-
-
-        console.log(markdownContent)
-
-       
-        const license = answer.license
-       
-       
-       
-       // // TODO: Create a function that returns the license link
-        // // If there is no license, return an empty string
-        function renderLicenseLink(license) {
-            if (license === 'Apache 2.0') {
-                return '(https://opensource.org/licenses/Apache-2.0)'
-            } else if (license === 'BSD') { 
-                return '(https://opensource.org/licenses/BSD-3-Clause)'
-                
-            }else if (license === 'MIT') { 
-                return '(https://opensource.org/licenses/MIT)'
-                
-            }else if (license === 'No License') { 
-                return ''
-            }
-        }
-
-       
-       
-       
-        // TODO: Create a function that returns a license badge based on which license is passed in
-        // If there is no license, return an empty string
-
-        function renderLicenseBadge(license) {
-
-            if (license !== 'None') {
-                return `![github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
-            }
-            return '';
-
-
-        }
-
-        // // TODO: Create a function that returns the license section of README
-        // // If there is no license, return an empty string
-        function renderLicenseSection(license) {
-            if (license === 'No Licence') {
-                return ''
-            } else {
-                return `This project is under the ${license} license: ${renderLicenseLink(license)}`
-            }
-        }
-
-
         // TODO: Create a function to write README file
-            fs.writeFile(fileName, markdownContent, (err) => {
+            fs.writeFile(fileName, generateMarkdown(answer), (err) => {
                 err ? console.log(err) : console.log('Success!')
             }
             )
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,33 +1,3 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// const license = answer.license
-
-// function renderLicenseBadge(license) {
-
-//   if (license !== 'None') {
-//     return `![github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
-//   }
-//   return '';
-
-
-// }
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) { }
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) { }
-
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-// `;
-// }
-
-
-
 // // TODO: Create a function that returns the license link
         // // If there is no license, return an empty string
         function renderLicenseLink(license) {
@@ -69,7 +39,59 @@
               return `This project is under the ${license} license: ${renderLicenseLink(license)}`
           }
       }
+    
+    function markdownGenerator(data) {     
+      
 
-module.exports = renderLicenseBadge();
-module.exports = renderLicenseLink();
-module.exports = renderLicenseSection();
+      // // TODO: Create a function to generate markdown for README
+      return
+          `
+      ${data.title}
+      ${(renderLicenseBadge(data.license))}
+
+
+      ## Table of Contents:
+      1. [Description](#description) 
+      2. [Installation](#installation)
+      3. [Usage](#usage)
+      4. [Contributing](#contributing)
+      5. [Testing](#testing)
+      6. [License](#license)
+      7. [Questions](#questions)
+
+
+      ## Description
+      ${data.description}
+
+
+      ## Installation
+      ${data.installation}
+
+      
+      ## Usage
+      ${data.usage}
+
+
+      ## Contributing
+      ${data.contributing}
+
+      
+      ## Testing
+      ${data.testing}
+
+
+      ## License
+      ${renderLicenseBadge(data.license)}
+      ${renderLicenseSection(data.license)}
+
+      
+      ## Questions
+      Contact me here with any questions about this project:
+      Github: ${data.github}
+      Email: ${data.email}
+
+      `;
+    }
+
+      module.exports = markdownGenerator
+
